6ac8c8357c7cc7ce0c51960f359f5447c408ea8a
IBRAHIM REFAT <101995954+ibrahimrifatcse@users.noreply.github.com>
2023-08-22 16:41:38 +0600

    Create All wrong.py

---
+++ WarmUp/All wrong.py
@@ -0,0 +1,9 @@
+# Write any import statements here
+
+def getWrongAnswers(N: int, C: str) -> str:
+     for i in range(N):
+       if C[i]=='A'
+          C[i]'B'
+        elif C[i]=='B'
+            C[i]='A'
+          return C
