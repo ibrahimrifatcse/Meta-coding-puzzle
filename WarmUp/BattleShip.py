dcef3b4c92f41ac13beaa35890aa8cb3e60b4343
IBRAHIM REFAT <101995954+ibrahimrifatcse@users.noreply.github.com>
2023-08-22 16:42:03 +0600

    Create BattleShip.py

---
+++ WarmUp/BattleShip.py
@@ -0,0 +1,7 @@
+from typing import List
+# Write any import statements here
+
+def getHitProbability(R: int, C: int, G: List[List[int]]) -> float:
+  # Write your code here
+  float k = R/C
+  return k
